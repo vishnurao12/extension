6adeb3b891954fe4e70991b4ef0f641f368ec938
Bharath Upadhya <bharathu@jfrog.com>
2020-06-03 00:27:18 +0530

    modify HealthCheck 0.0.2

--- steps/HealthCheck/0.0.1/onExecute.sh
+++ steps/HealthCheck/0.0.1/onExecute.sh
@@ -1,8 +1,10 @@
 checkHealth() {
   local url=$(find_step_configuration_value "healthCheckUrl")
-  statusCode=$(curl --silent --output /dev/stderr --write-out "%{http_code}" "$url")
+  {
+    statusCode=$(curl --silent --output /dev/stderr --write-out "%{http_code}" "$url")
+  } || exitCode=$?
   if test $statusCode -ne 200; then
-    echo "Health check failed with statusCode: $statusCode for url: $url"
+    echo "Health check failed with statusCode: $statusCode & exitCode: $exitCode for url: $url"
     exit 1
   else
     echo "Health check succeeded"
